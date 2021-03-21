f2f21c849f86d4dedafb39afef909787b25ecb9c
Nikita Fediuchin <cfnptr@gmail.com>
2021-03-21 11:21:25 +0200

    Rename angle functions

--- include/cmmt/angle.h
+++ include/cmmt/angle.h
@@ -1,13 +1,24 @@
 #pragma once
 #include <math.h>
 
-inline static double angleToDeg(
+inline static double radToDegD(
 	double radians)
 {
 	return radians * (180.0 / M_PI);
 }
-inline static double angleToRad(
+inline static float radToDegF(
+	float radians)
+{
+	return radians * (180.0f / M_PI);
+}
+
+inline static double degToRadD(
 	double degrees)
+{
+	return degrees * (M_PI / 180.0);
+}
+inline static float degToRadF(
+	float degrees)
 {
 	return degrees * (M_PI / 180.0f);
 }
