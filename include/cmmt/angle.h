f7fc211c9a2a4f6711e91befcf2d322f92349e2c
Nikita Fediuchin <cfnptr@gmail.com>
2021-07-25 21:53:11 +0300

    Refactor function style

--- include/cmmt/angle.h
+++ include/cmmt/angle.h
@@ -2,24 +2,20 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 
-inline static double radToDegD(
-	double radians)
+inline static double radToDegD(double radians)
 {
 	return radians * (180.0 / M_PI);
 }
-inline static float radToDegF(
-	float radians)
+inline static float radToDegF(float radians)
 {
 	return radians * (180.0f / (float)M_PI);
 }
 
-inline static double degToRadD(
-	double degrees)
+inline static double degToRadD(double degrees)
 {
 	return degrees * (M_PI / 180.0);
 }
-inline static float degToRadF(
-	float degrees)
+inline static float degToRadF(float degrees)
 {
 	return degrees * ((float)M_PI / 180.0f);
 }
--- include/cmmt/color.h
+++ include/cmmt/color.h
@@ -7,17 +7,14 @@ inline static float colToVal(
 	return (float)r / 255.0f;
 }
 inline static Vec2F colToVec2F(
-	uint8_t r,
-	uint8_t g)
+	uint8_t r, uint8_t g)
 {
 	return vec2F(
 		(float)r / 255.0f,
 		(float)g / 255.0f);
 }
 inline static Vec3F colToVec3F(
-	uint8_t r,
-	uint8_t g,
-	uint8_t b)
+	uint8_t r, uint8_t g, uint8_t b)
 {
 	return vec3F(
 		(float)r / 255.0f,
@@ -25,10 +22,7 @@ inline static Vec3F colToVec3F(
 		(float)b / 255.0f);
 }
 inline static Vec4F colToVec4F(
-	uint8_t r,
-	uint8_t g,
-	uint8_t b,
-	uint8_t a)
+	uint8_t r, uint8_t g, uint8_t b, uint8_t a)
 {
 	return vec4F(
 		(float)r / 255.0f,
